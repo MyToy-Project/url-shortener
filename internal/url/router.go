d419c0ab813627bd678fb247b3aaf5e0a44ae602
hoseok.lee <hoseok.lee@deliveryhero.com>
2026-01-06 00:59:30 +0100

    Add test log

--- internal/url/router.go
+++ internal/url/router.go
@@ -244,6 +244,7 @@ func rateLimitByIP(rpm int, burst int) func(http.Handler) http.Handler {
 // If behind a trusted proxy, ensure your proxy sets X-Forwarded-For or X-Real-IP.
 func clientIP(r *http.Request) string {
 	// X-Forwarded-For may contain multiple IPs: client, proxy1, proxy2, ...
+	slog.Default().Info("all ips", "ips", r.Header)
 	xff := r.Header.Get("X-Forwarded-For")
 	if xff != "" {
 		parts := strings.Split(xff, ",")
